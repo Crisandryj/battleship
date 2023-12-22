ac0e6f73f3f43127988449c5688a47364c9f0733
Crisandry Javier <crisandryj@gmail.com>
2023-12-21 19:53:57 -0500

    Feat: Create ship class with size constructor

--- src/myName.js
+++
@@ -1,7 +0,0 @@
-const myName = (name) => "Hi! My name is " + name;
-
-const sum = function sum(a, b) {
-  return a + b;
-};
-
-export { myName, sum };
---
+++ src/ships.js
@@ -0,0 +1,9 @@
+const sum = function sum(a, b) {
+  return a + b;
+};
+class Ships {
+  constructor(length) {
+    this.length = length;
+  }
+}
+export { sum };
