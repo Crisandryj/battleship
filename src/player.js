0b5381d411d92bd89bd39fdaf72a18a15e68a425
Crisandry Javier <crisandryj@gmail.com>
2023-12-28 11:09:34 -0500

    Feat: Add while loop until coordinates found

--- src/player.js
+++ src/player.js
@@ -9,13 +9,21 @@ class Player {
   }
 
   computerMove(board) {
-    let column = Math.floor(Math.random() * 10);
-    let row = Math.floor(Math.random() * 10);
-    if (board[column][row].hit == false && board[column][row].missed == false) {
-      if (board[column][row].occupied == true) {
-        board[column][row].hit = true;
-      } else {
-        board[column][row].missed = true;
+    let complete = false;
+    while ((complete = false)) {
+      let column = Math.floor(Math.random() * 10);
+      let row = Math.floor(Math.random() * 10);
+      if (
+        board[column][row].hit == false &&
+        board[column][row].missed == false
+      ) {
+        if (board[column][row].occupied == true) {
+          board[column][row].hit = true;
+          complete = true;
+        } else {
+          board[column][row].missed = true;
+          complete = true;
+        }
       }
     }
   }
