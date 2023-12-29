e72bf35abc887ded69cd8585ce1011fa1a5e1e5b
Crisandry Javier <crisandryj@gmail.com>
2023-12-29 10:52:36 -0500

    Feat: Run webpack build

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import { forEach } from "lodash";
+const { Gameboard } = require("../src/gameBoard");
+const { Player } = require("../src/player");
 const { Cell } = require("../src/cells");
 const { Ship } = require("../src/ships");
-const { Player } = require("../src/player");
-const { Gameboard } = require("../src/gameBoard");
 
-function playGame() {
-  const game = new Gameboard();
-  const playerOne = new Player("James");
-  const computer = new Player("Comp");
-
-  game.createBoard();
-}
-
-document.body.appendChild(component());
+console.log("Hello");
