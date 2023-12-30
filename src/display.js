61ab14eec9bf30e7e998fc3c4468054982a5e428
Crisandry Javier <crisandryj@gmail.com>
2023-12-30 12:51:10 -0500

    Feat: Add rows to table

--- src/display.js
+++ src/display.js
@@ -1,6 +1,13 @@
 const table = document.createElement("Table");
 const body = document.querySelector("body");
 
+function addRow(n, table) {
+  for (let i = 0; i < n; i++) {
+    table.insertRow(-1);
+  }
+}
+
 export function renderTable() {
   body.appendChild(table);
+  addRow(10, table);
 }
