87d40db8600f50b850294120097475e1a67a7ad9
Crisandry Javier <crisandryj@gmail.com>
2024-01-08 16:23:57 -0500

    Add comments for playgame

--- src/index.js
+++ src/index.js
@@ -7,17 +7,19 @@ const { renderBoard, renderAttack } = require("../src/display");
 function playGame() {
   const playerOne = new Player("P1");
   const computer = new Player("Comp");
+  //place player ships
   playerOne.game.placeShip(5, "v", 2, 2);
   playerOne.game.placeShip(4, "v", 3, 2);
   playerOne.game.placeShip(3, "v", 4, 2);
   playerOne.game.placeShip(3, "v", 5, 2);
   playerOne.game.placeShip(2, "v", 6, 2);
-
+  //place computer ships
   computer.game.placeShip(5, "v", 3, 2);
   computer.game.placeShip(4, "v", 4, 2);
   computer.game.placeShip(3, "v", 5, 2);
   computer.game.placeShip(3, "v", 6, 2);
   computer.game.placeShip(2, "v", 7, 2);
+  //display boards
   renderBoard(playerOne.game.board, "p1Board");
   renderBoard(computer.game.board, "compBoard");
   prompt("PlayerOne place attack");
