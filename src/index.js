afac9d6b61be7aad1a35b49412d038b514b38018
Crisandry Javier <crisandryj@gmail.com>
2024-01-10 11:29:17 -0500

    Feat: Create function to default ships locations in the begining of game

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ const { Cell } = require("../src/cells");
 const { Ship } = require("../src/ships");
 const { renderBoard, renderAttack } = require("../src/display");
 
-function playGame() {
-  const playerOne = new Player("P1");
-  const computer = new Player("Comp");
-  playerOne.turn = true;
+function defaultPlaceShips(playerOne, computer) {
   //place player ships
   playerOne.game.placeShip(5, "v", 2, 2);
   playerOne.game.placeShip(4, "v", 3, 2);
@@ -20,6 +17,15 @@ function playGame() {
   computer.game.placeShip(3, "v", 5, 2);
   computer.game.placeShip(3, "v", 6, 2);
   computer.game.placeShip(2, "v", 7, 2);
+}
+
+function playGame() {
+  const playerOne = new Player("P1");
+  const computer = new Player("Comp");
+  playerOne.turn = true;
+
+  defaultPlaceShips(playerOne, computer);
+
   //display boards
   renderBoard(playerOne.game.board, "p1Board");
   renderBoard(computer.game.board, "compBoard");
