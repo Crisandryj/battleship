c45f82d72361d6cf4aeccc50a89827ab73f532bb
Crisandry Javier <crisandryj@gmail.com>
2024-01-29 12:36:07 -0500

    Feat: Change div to gameboard container/ funciton creates game board div wit color and adds to container

--- src/display.js
+++ src/display.js
@@ -2,11 +2,14 @@ const body = document.querySelector("body");
 const { Gameboard } = require("../src/gameBoard");
 const { Player } = require("../src/player");
 
-const gameBoard = document.querySelector(".game-board");
+const gameBoardsContainer = document.querySelector(".game-boards-container");
 
 let count = 0;
 export function displayBoard(player, color) {
+  const gameBoard = document.createElement("div");
+  gameBoard.classList.add("game-board");
   gameBoard.style.backgroundColor = color;
+  gameBoardsContainer.append(gameBoard);
   player.game.board.forEach((row) => {
     row.forEach((item) => {
       const block = document.createElement("div");
