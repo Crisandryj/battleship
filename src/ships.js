57d97e2c2486fa37c4b69ebb17d552311255416b
Crisandry Javier <crisandryj@gmail.com>
2024-02-10 22:29:00 -0500

    Fix: test checks for obect with miss and occupied props

--- src/ships.js
+++ src/ships.js
@@ -1,11 +1,20 @@
 export class Ship {
-  constructor(name, length) {
-    this.length = length;
+  constructor(name, len) {
+    this.array = [];
+    this.length = this.array.length;
     this.name = name;
     this.hits = 0;
     this.occupied = true;
+    for (let i = 0; i < len; i++) {
+      this.array.push(i);
+    }
   }
 
+  shipLength(len) {
+    for (let i = 0; i < length; i++) {
+      this.array.push(i);
+    }
+  }
   hit() {
     this.hits += 1;
     return this.hits;
