c1c6005f6631c303022296b81be1a1211d38380e
Crisandry Javier <crisandryj@gmail.com>
2024-02-12 15:43:09 -0500

    Feat: Each ship object has an array

--- src/index.js
+++ src/index.js
@@ -86,3 +86,5 @@ function selectAttack(evt) {
     }
   }
 }
+
+console.log(bigOne);
--- src/ships.js
+++ src/ships.js
@@ -1,13 +1,13 @@
 export class Ship {
   constructor(name, len) {
     this.array = [];
+    for (let i = 0; i < len; i++) {
+      this.array.push(i);
+    }
     this.length = this.array.length;
     this.name = name;
     this.hits = 0;
     this.occupied = true;
-    for (let i = 0; i < len; i++) {
-      this.array.push(i);
-    }
   }
 
   shipLength(len) {
