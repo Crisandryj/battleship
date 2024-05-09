5f7bf47c4af8b5bce5f353cbd0bfb671fd337735
Crisandry Javier <crisandryj@gmail.com>
2024-05-09 11:04:52 -0400

    Refactor: Move startGame, handleClick and selectAttackt to module dom

---
+++ src/dom.js
@@ -0,0 +1,81 @@
+const { displayBoard } = require("../src/display");
+
+export function startGame(
+  GamesContainer,
+  playerOne,
+  playerTwo,
+  display,
+  currentBoard
+) {
+  GamesContainer.removeChild(GamesContainer.firstElementChild);
+  playerOne.turn = true;
+  display.textContent = "P1 Select Target";
+  displayBoard(playerOne, playerTwo, "gray", "blue");
+  currentBoard = document.querySelector(".P2-board");
+  currentBoard.addEventListener("click", handleClick);
+}
+
+let toggleClass = ".P2-board";
+
+function handleClick(evt) {
+  if (evt.target.closest("div") === null) {
+    return;
+  } else {
+    selectAttack(evt, playerOne, playerTwo);
+    playerTwo.changeTurn();
+    playerOne.changeTurn();
+    switchBoard(playerOne, playerTwo);
+    if (toggleClass == ".P1-board") {
+      toggleClass = ".P2-board";
+      console.log("p2");
+    } else {
+      toggleClass = ".P1-board";
+      console.log("p1");
+    }
+    currentBoard = document.querySelector(toggleClass);
+    console.log(currentBoard);
+  }
+}
+
+function selectAttack(evt, playerOne, playerTwo) {
+  let rowNum = evt.target.closest("div").id[0];
+  let columnNum = evt.target.closest("div").id[1];
+
+  switch (columnNum) {
+    case undefined:
+      if (playerOne.game.board[0][rowNum].missed == true) {
+        switchBoard(playerOne, playerTwo);
+        return;
+      }
+      if (playerOne.turn == false) {
+        playerOne.game.recieveAttack(0, rowNum, evt.target.closest("div").id);
+
+        gameOver(playerOne);
+      } else {
+        playerTwo.game.recieveAttack(0, rowNum, evt.target.closest("div").id);
+
+        gameOver(playerTwo);
+      }
+      break;
+    default:
+      if (playerOne.game.board[columnNum][0].missed == true) {
+        switchBoard(playerOne, playerTwo);
+        return;
+      }
+      if (playerOne.turn == false) {
+        playerOne.game.recieveAttack(
+          rowNum,
+          columnNum,
+          evt.target.closest("div").id
+        );
+        gameOver(playerOne);
+      } else {
+        playerTwo.game.recieveAttack(
+          rowNum,
+          columnNum,
+          evt.target.closest("div").id
+        );
+        gameOver(playerTwo);
+      }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const {
   colorSquares,
   gameOver,
 } = require("../src/display");
+const { startGame } = require("../src/dom");
 
 const display = document.querySelector(".display");
 
@@ -37,35 +38,6 @@ displaySetUpBoard(playerOne);
 //select first board
 let gameBoard = document.querySelector(".P1-board");
 let currentBoard;
-function startGame() {
-  start = true;
-  GamesContainer.removeChild(GamesContainer.firstElementChild);
-  playerOne.turn = true;
-  display.textContent = "P1 Select Target";
-  displayBoard(playerOne, playerTwo, "gray", "blue");
-  currentBoard = document.querySelector(".P2-board");
-  currentBoard.addEventListener("click", handleClick);
-}
-let toggleClass = ".P2-board";
-function handleClick(evt) {
-  if (evt.target.closest("div") === null) {
-    return;
-  } else {
-    selectAttack(evt);
-    playerTwo.changeTurn();
-    playerOne.changeTurn();
-    switchBoard(playerOne, playerTwo);
-    if (toggleClass == ".P1-board") {
-      toggleClass = ".P2-board";
-      console.log("p2");
-    } else {
-      toggleClass = ".P1-board";
-      console.log("p1");
-    }
-    currentBoard = document.querySelector(toggleClass);
-    console.log(currentBoard);
-  }
-}
 
 function checkShipPlaced(player, length) {
   let check = false;
@@ -77,49 +49,6 @@ function checkShipPlaced(player, length) {
     }
   });
   return check;
-}
-
-function selectAttack(evt) {
-  let rowNum = evt.target.closest("div").id[0];
-  let columnNum = evt.target.closest("div").id[1];
-
-  switch (columnNum) {
-    case undefined:
-      if (playerOne.game.board[0][rowNum].missed == true) {
-        switchBoard(playerOne, playerTwo);
-        return;
-      }
-      if (playerOne.turn == false) {
-        playerOne.game.recieveAttack(0, rowNum, evt.target.closest("div").id);
-
-        gameOver(playerOne);
-      } else {
-        playerTwo.game.recieveAttack(0, rowNum, evt.target.closest("div").id);
-
-        gameOver(playerTwo);
-      }
-      break;
-    default:
-      if (playerOne.game.board[columnNum][0].missed == true) {
-        switchBoard(playerOne, playerTwo);
-        return;
-      }
-      if (playerOne.turn == false) {
-        playerOne.game.recieveAttack(
-          rowNum,
-          columnNum,
-          evt.target.closest("div").id
-        );
-        gameOver(playerOne);
-      } else {
-        playerTwo.game.recieveAttack(
-          rowNum,
-          columnNum,
-          evt.target.closest("div").id
-        );
-        gameOver(playerTwo);
-      }
-  }
 }
 
 function chooseBoard(length, turn, columnNum, rowNum) {
@@ -155,7 +84,7 @@ function appendDoneListener() {
     }
     count = 0;
     if (doneCount == 2) {
-      startGame();
+      startGame(GamesContainer, playerOne, playerTwo, display, currentBoard);
       removeBtns();
       doneCount = 0;
     }
