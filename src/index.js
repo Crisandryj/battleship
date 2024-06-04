afd9a77a929b111d3408e15789aaaa8cca169750
Crisandry Javier <crisandryj@gmail.com>
2024-06-04 11:35:34 -0400

    Feat: Add startBattle function with logic moved into it

--- src/index.js
+++ src/index.js
@@ -19,23 +19,13 @@ const buttons = document.querySelector(".buttons");
 const playerOne = new Player("P1");
 const playerTwo = new Player("P2");
 
-//defaultPlaceShips(playerOne, playerTwo)
-const turnButton = document.querySelector("#turnShip");
 //handle turn of ships
 let turn = false;
-
-//display set up board
-displaySetUpBoard(playerOne);
-
-//select first board
-let gameBoard = document.querySelector(".P1-board");
 
 //place ship random location
 const random = document.createElement("BUTTON");
 random.classList.add("random");
 random.textContent = "Random Placement";
-
-buttons.append(random);
 
 //place a ship randomly on board
 function randomShip(length, count) {
@@ -61,125 +51,145 @@ function randomShip(length, count) {
 let count = 0;
 let doneCount = 0;
 
-turnButton.addEventListener("click", () => {
-  return turn ? (turn = false) : (turn = true);
-});
+const turnButton = document.createElement("BUTTON");
+const startBattleGame = document.createElement("BUTTON");
+startBattleGame.textContent = "START";
 
-gameBoard.addEventListener("click", (evt) => {
-  let rowNum;
-  let columnNum;
-  if (evt.target.closest("div").id[1] >= 0) {
-    columnNum = evt.target.closest("div").id[0];
-    rowNum = evt.target.closest("div").id[1];
-  } else {
-    rowNum = evt.target.closest("div").id[0];
-    columnNum = 0;
-  }
-  //place ships
-  // go in order of ships and allow 'X' consective clicks in order to place ships in place
-  switch (count) {
-    case 0:
-      const bigOne = new Ship(5);
-      chooseBoard(
-        bigOne.length,
-        turn,
-        columnNum,
-        rowNum,
-        playerOne,
-        playerTwo,
-        gameBoard
-      );
+buttons.append(startBattleGame);
 
-      colorSquares(bigOne, evt, turn);
+startBattleGame.addEventListener("click", startBattle);
 
-      if (checkShipPlaced(playerOne, bigOne.length)) {
-        count += 1;
-        console.log("check");
-      }
+function startBattle() {
+  //add turnship button
+  turnButton.textContent = "Turn Ship";
+  turnButton.setAttribute("id", "turnButton");
+  buttons.append(turnButton);
+  // add random ship placement button
+  buttons.append(random);
+  //display set up board
+  displaySetUpBoard(playerOne);
+  //select first board
+  let gameBoard = document.querySelector(".P1-board");
 
-      break;
-    case 1:
-      const scarey = new Ship(4);
-      //not getting called?
-      randomShip(scarey.length, count);
-      chooseBoard(
-        scarey.length,
-        turn,
-        columnNum,
-        rowNum,
-        playerOne,
-        playerTwo,
-        gameBoard
-      );
+  turnButton.addEventListener("click", () => {
+    return turn ? (turn = false) : (turn = true);
+  });
 
-      colorSquares(scarey, evt, turn);
-      if (checkShipPlaced(playerOne, scarey.length)) {
-        console.log("check 2");
-        count += 1;
-      }
-      break;
-    case 2:
-      const middleChild = new Ship(3);
-      chooseBoard(
-        middleChild.length,
-        turn,
-        columnNum,
-        rowNum,
-        playerOne,
-        playerTwo,
-        gameBoard
-      );
-      colorSquares(middleChild, evt, turn);
-      if (checkShipPlaced(playerOne, middleChild.length)) {
-        count += 1;
-      }
-      break;
-    case 3:
-      const sneakyPlayer = new Ship(3);
-      chooseBoard(
-        sneakyPlayer.length,
-        turn,
-        columnNum,
-        rowNum,
-        playerOne,
-        playerTwo,
-        gameBoard
-      );
-      colorSquares(sneakyPlayer, evt, turn);
-      if (checkShipPlaced(playerOne, sneakyPlayer.length)) {
-        count += 1;
-      }
+  gameBoard.addEventListener("click", (evt) => {
+    let rowNum;
+    let columnNum;
+    if (evt.target.closest("div").id[1] >= 0) {
+      columnNum = evt.target.closest("div").id[0];
+      rowNum = evt.target.closest("div").id[1];
+    } else {
+      rowNum = evt.target.closest("div").id[0];
+      columnNum = 0;
+    }
+    //place ships
+    // go in order of ships and allow 'X' consective clicks in order to place ships in place
+    switch (count) {
+      case 0:
+        const bigOne = new Ship(5);
+        chooseBoard(
+          bigOne.length,
+          turn,
+          columnNum,
+          rowNum,
+          playerOne,
+          playerTwo,
+          gameBoard
+        );
 
-      break;
-    case 4:
-      const littleOne = new Ship(2);
-      chooseBoard(
-        littleOne.length,
-        turn,
-        columnNum,
-        rowNum,
-        playerOne,
-        playerTwo,
-        gameBoard
-      );
-      colorSquares(littleOne, evt, turn);
-      if (checkShipPlaced(playerOne, littleOne.length)) {
-        count += 1;
-      }
+        colorSquares(bigOne, evt, turn);
 
-      appendDoneListener(gameBoard);
+        if (checkShipPlaced(playerOne, bigOne.length)) {
+          count += 1;
+          console.log("check");
+        }
 
-      if (doneCount <= 1) {
-        doneCount += 1;
-        count = 0;
-      }
-      if (doneCount >= 2) {
-        startGame(GamesContainer, playerOne, playerTwo, display);
-        const doneBtn = document.querySelector(".done");
-        removeBtns(doneBtn, turnButton);
-        doneCount = 0;
-      }
-      //Finish board set up and move to next board
-      break;
-  }
-});
+        break;
+      case 1:
+        const scarey = new Ship(4);
+        //not getting called?
+        chooseBoard(
+          scarey.length,
+          turn,
+          columnNum,
+          rowNum,
+          playerOne,
+          playerTwo,
+          gameBoard
+        );
+
+        colorSquares(scarey, evt, turn);
+        if (checkShipPlaced(playerOne, scarey.length)) {
+          console.log("check 2");
+          count += 1;
+        }
+        break;
+      case 2:
+        const middleChild = new Ship(3);
+        chooseBoard(
+          middleChild.length,
+          turn,
+          columnNum,
+          rowNum,
+          playerOne,
+          playerTwo,
+          gameBoard
+        );
+        colorSquares(middleChild, evt, turn);
+        if (checkShipPlaced(playerOne, middleChild.length)) {
+          count += 1;
+        }
+        break;
+      case 3:
+        const sneakyPlayer = new Ship(3);
+        chooseBoard(
+          sneakyPlayer.length,
+          turn,
+          columnNum,
+          rowNum,
+          playerOne,
+          playerTwo,
+          gameBoard
+        );
+        colorSquares(sneakyPlayer, evt, turn);
+        if (checkShipPlaced(playerOne, sneakyPlayer.length)) {
+          count += 1;
+        }
+
+        break;
+      case 4:
+        const littleOne = new Ship(2);
+        chooseBoard(
+          littleOne.length,
+          turn,
+          columnNum,
+          rowNum,
+          playerOne,
+          playerTwo,
+          gameBoard
+        );
+        colorSquares(littleOne, evt, turn);
+        if (checkShipPlaced(playerOne, littleOne.length)) {
+          count += 1;
+        }
+
+        appendDoneListener(gameBoard);
+
+        if (doneCount <= 1) {
+          doneCount += 1;
+          count = 0;
+        }
+        if (doneCount >= 2) {
+          startGame(GamesContainer, playerOne, playerTwo, display);
+          const doneBtn = document.querySelector(".done");
+          removeBtns(doneBtn, turnButton);
+          doneCount = 0;
+        }
+        //Finish board set up and move to next board
+        break;
+    }
+  });
+}
